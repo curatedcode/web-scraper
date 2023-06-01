abc602a9484c7e57fbe2a17946de8f1d160bf2f0
Zack F <zackaryfotheringham@hotmail.com>
2023-06-01 01:27:56 -0400

    add crawlers for all categories

--- src/crawlerController.ts
+++ src/crawlerController.ts
@@ -1,4 +1,25 @@
 import { Browser } from "puppeteer";
+import crawler from "./crawler";
+import {
+  automotiveUrls,
+  babyUrls,
+  beautyUrls,
+  bookUrls,
+  clothingUrls,
+  computersUrls,
+  electronicsUrls,
+  gamesUrls,
+  gardenUrls,
+  healthUrls,
+  homeUrls,
+  jewelryUrls,
+  kidsUrls,
+  movieUrls,
+  musicUrls,
+  outdoorUrls,
+  sportUrls,
+  toolsUrls,
+} from "./productUrls";
 
 async function crawlerController(
   browserInstance: Promise<Browser | undefined>
@@ -7,6 +28,96 @@ async function crawlerController(
   if (!browser) throw new Error("Browser instance is undefined");
 
   try {
+    await crawler({
+      name: "book",
+      browser,
+      productUrls: bookUrls,
+    });
+    await crawler({
+      name: "movies",
+      browser,
+      productUrls: movieUrls,
+    });
+    await crawler({
+      name: "music",
+      browser,
+      productUrls: musicUrls,
+    });
+    await crawler({
+      name: "beauty",
+      browser,
+      productUrls: beautyUrls,
+    });
+    await crawler({
+      name: "sports",
+      browser,
+      productUrls: sportUrls,
+    });
+    await crawler({
+      name: "jewelry",
+      browser,
+      productUrls: jewelryUrls,
+    });
+    await crawler({
+      name: "home",
+      browser,
+      productUrls: homeUrls,
+    });
+    await crawler({
+      name: "electronics",
+      browser,
+      productUrls: electronicsUrls,
+    });
+    await crawler({
+      name: "computers",
+      browser,
+      productUrls: computersUrls,
+    });
+    await crawler({
+      name: "kids",
+      browser,
+      productUrls: kidsUrls,
+    });
+    await crawler({
+      name: "baby",
+      browser,
+      productUrls: babyUrls,
+    });
+    await crawler({
+      name: "automotive",
+      browser,
+      productUrls: automotiveUrls,
+    });
+    await crawler({
+      name: "clothing",
+      browser,
+      productUrls: clothingUrls,
+    });
+    await crawler({
+      name: "garden",
+      browser,
+      productUrls: gardenUrls,
+    });
+    await crawler({
+      name: "outdoor",
+      browser,
+      productUrls: outdoorUrls,
+    });
+    await crawler({
+      name: "games",
+      browser,
+      productUrls: gamesUrls,
+    });
+    await crawler({
+      name: "health",
+      browser,
+      productUrls: healthUrls,
+    });
+    await crawler({
+      name: "tools",
+      browser,
+      productUrls: toolsUrls,
+    });
     console.log("Done crawling all pages");
   } catch (err) {
     console.log(`Error scraping data: ${err}`);
