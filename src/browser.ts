7b9060913ee8eb8edcc1b9c32dd246fcdfbecb77
Zack F <zackaryfotheringham@hotmail.com>
2023-06-01 00:53:35 -0400

    add methods to avoid being flagged by site

--- src/browser.ts
+++ src/browser.ts
@@ -1,9 +1,14 @@
-import puppeteer, { Browser, PuppeteerLaunchOptions } from "puppeteer";
+import { Browser, PuppeteerLaunchOptions } from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import stealthPlugin from "puppeteer-extra-plugin-stealth";
+puppeteer.use(stealthPlugin());
 
 const browserOptions: PuppeteerLaunchOptions = {
   headless: false,
   args: ["--disable-setuid-sandbox"],
   ignoreHTTPSErrors: true,
+  defaultViewport: { height: 1080, width: 1920 },
+  executablePath: "/usr/bin/google-chrome-stable",
 };
 
 async function startBrowser(): Promise<Browser | undefined> {
